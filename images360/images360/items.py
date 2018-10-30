0a7b02e6358b0f1d8811eee9ae8c5a56997fde94
cxc <522467360@qq.com>
2018-10-30 10:34:15 +0800

    add scrapy-images360

---
+++ images360/images360/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Item, Field
+
+class Images360Item(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    collection = table = '360images'
+    id = Field()
+    url = Field()
+    title = Field()
+    thumb = Field()
---
+++ images360/images360/pipelines.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+
+# class Images360Pipeline(object):
+#     def process_item(self, item, spider):
+#         return item
+
+class MysqlPipeline():
+    def __init__(self, host, database, user, password, port):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.port = port
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            host = crawler.settings.get('MYSQL_HOST'),
+            database = crawler.settings.get('MYSQL_DATABASE'),
+            user = crawler.settings.get('MYSQL_USER'),
+            password = crawler.settings.get('MYSQL_PASSWORD'),
+            port = crawler.settings.get('MYSQL_PORT'),
+        )
+
+    def open_spider(self, spider):
+        self.db = pymysql.connect(self.host, self.user, self.password, self.database, charset = 'utf8', port = self.port)
+        self.cursor = self.db.cursor()
+
+    def close_spider(self, spider):
+        self.db.close()
+
+    def process_item(self, item, spider):
+        data = dict(item)
+        keys = ','.join(data.keys())
+        values = ','.join(['%s'] * len(data))
+        sql = 'insert into %s (%s) values (%s)' % (item.table, keys, values)
+        self.cursor.execute(sql, tuple(data.values()))
+        self.db.commit()
+        return item
+
+from scrapy import Request
+from scrapy.exceptions import DropItem
+from scrapy.pipelines.images import ImagesPipeline
+
+class ImagePipeline(ImagesPipeline):
+    def file_path(self, request, response=None, info=None):
+        url = request.url
+        file_name = url.split('/')[-1]
+        return file_name
+
+    def item_completed(self, results, item, info):
+        image_paths = [x['path'] for ok, x in results if ok]
+        if not image_paths:
+            raise DropItem('Image Downloaded Failed')
+        return item
+
+    def get_media_requests(self, item, info):
+        yield  Request(item['url'])
